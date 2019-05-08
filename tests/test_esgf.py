c302b9582b5589995cee42ad981e7140df6a1433
Pingu Carsti <ehbrecht@dkrz.de>
2019-05-08 16:17:12 +0200

    pep8

--- tests/test_esgf.py
+++ tests/test_esgf.py
@@ -1,18 +1,18 @@
 import pytest
 import unittest
-# import mock
 from tempfile import mkdtemp
 
 from twitcher.esgf import ESGFAccessManager
 
 
+@pytest.mark.skip(reason="fix test")
 class ESGFTestCase(unittest.TestCase):
     def setUp(self):
+        import mock
         self.mgr = ESGFAccessManager(
             slcs_service_url="https://localhost:5000",
             base_dir=mkdtemp())
         self.mgr._retrieve_certificate = mock.MagicMock(return_value=True)
 
-    @pytest.mark.skip(reason="fix test")
     def test_logon_with_token(self):
         assert self.mgr.logon(access_token="abcdef") is True
