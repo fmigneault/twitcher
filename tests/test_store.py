8e65ab7232234c35351d50451bf3aeec1e2067fe
Pingu Carsti <ehbrecht@dkrz.de>
2019-05-08 18:17:03 +0200

    update service store

--- tests/test_store.py
+++ tests/test_store.py
@@ -7,7 +7,7 @@ from .common import BaseTest, dummy_request
 
 from twitcher.utils import expires_at
 from twitcher.store import AccessTokenStore, ServiceStore
-from twitcher.models import AccessToken, Service
+from twitcher.datatype import AccessToken, Service
 
 
 class AccessTokenStoreTestCase(BaseTest):
@@ -18,7 +18,7 @@ class AccessTokenStoreTestCase(BaseTest):
         self.token_store = AccessTokenStore(
             dummy_request(dbsession=self.session))
 
-    def test_token(self):
+    def test_token_store(self):
         self.token_store.save_token(
             AccessToken(token="abc", expires_at=expires_at(hours=1)))
         token = self.token_store.fetch_by_token(token='abc')
@@ -35,17 +35,22 @@ class ServiceStoreTestCase(BaseTest):
         self.service_store = ServiceStore(
             dummy_request(dbsession=self.session))
 
-    def test_fetch_by_name(self):
-        pass
-
-    def test_save_service(self):
+    def test_service_store(self):
         self.service_store.save_service(
             Service(
-                name="loving_flamingo",
+                name="flamingo",
                 url="http://somewhere.over.the/ocean",
                 type="wps",
-                # public=False,
+                public=False,
                 auth='token',
-                # verify=True,
+                verify=True,
                 purl="http://purl/wps")
         )
+        service = self.service_store.fetch_by_name(name='flamingo')
+        assert service.name == 'flamingo'
+        service = self.service_store.fetch_by_url(url='http://somewhere.over.the/ocean')
+        assert service.url == 'http://somewhere.over.the/ocean'
+        services = self.service_store.list_services()
+        assert len(services) == 1
+        self.service_store.delete_service(name='flamingo')
+        self.service_store.clear_services()
