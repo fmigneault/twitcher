be8a126cc757bc74b6a35287b185e98474440d92
Pingu Carsti <ehbrecht@dkrz.de>
2019-05-10 16:09:05 +0200

    skip prepare header

--- twitcher/owssecurity.py
+++ twitcher/owssecurity.py
@@ -41,15 +41,6 @@ class OWSSecurity(object):
                 token = elements[-1]   # last path element
         return token
 
-    def prepare_headers(self, request, access_token):
-        if "esgf_access_token" in access_token.data or "esgf_credentials" in access_token.data:
-            workdir = tempfile.mkdtemp(prefix=request.prefix, dir=request.workdir)
-            if fetch_certificate(workdir=workdir, data=access_token.data):
-                request.headers['X-Requested-Workdir'] = workdir
-                request.headers['X-X509-User-Proxy'] = workdir + '/' + ESGF_CREDENTIALS
-                LOGGER.debug("Prepared request headers.")
-        return request
-
     def verify_access(self, request, service):
         # TODO: public service access handling is confusing.
         try:
@@ -68,10 +59,6 @@ class OWSSecurity(object):
             access_token = self.tokenstore.fetch_by_token(token)
             if access_token.is_expired():
                 raise OWSAccessForbidden("Access token is expired.")
-            # update request with data from access token
-            # request.environ.update(access_token.data)
-            # TODO: is this realy the way we want to do this?
-            request = self.prepare_headers(request, access_token)
         except AccessTokenNotFound:
             raise OWSAccessForbidden("Access token is required to access this service.")
 
